d92e7b07fe5061f469afab4ef02167d3df69a670
Richard <richardchoi54@gmail.com>
2024-12-03 13:36:56 -0500

    Grab user data to frontend
    display user email, name, likes, hobbies, and interests to frontend. Remove logo tentatively. Add light layout to profile page.

--- resources/js/Pages/Profile/Show.jsx
+++ resources/js/Pages/Profile/Show.jsx
@@ -1,16 +1,37 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
+import { Link, useForm, usePage } from '@inertiajs/react';
 
 export default function Show() {
+    const user = usePage().props.auth.user;
+
+    const { data, setData, patch, errors, processing, recentlySuccessful } =
+        useForm({
+            name: user.name,
+            email: user.email,
+            likes: user.likes,
+            interests: user.interests,
+            hobbies: user.hobbies
+        });
+
     return (
         <AuthenticatedLayout
             header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                    Profile
+                <h2 className="capitalize text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                    {data.name}'s Profile
                 </h2>
             }
         >
             <Head title="Profile" />
+            <section className="flex flex-col items-start p-10">
+            <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Email: {data.email}</h2>
+            <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Likes: {data.likes}</h2>
+            <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Hobbies: {data.hobbies}</h2>
+            <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Interests: {data.interests}</h2>
+            </section>
+
+
+
         </AuthenticatedLayout>
     );
 }
